64c2e4827e455c45da9f140e7a574319f37084df
lsilvest <lsilvestri@ztsdb.org>
2017-02-28 03:23:47 -0500

    allow expression lists to be sent over to ztsdb from R

--- R/packages/ztsdb/R/ztsdb.R
+++ R/packages/ztsdb/R/ztsdb.R
@@ -2,7 +2,7 @@ setGeneric("?", function(e1, e2) standardGeneric("?"))
 setMethod("?",
           signature("Rcpp_Zconnection"),
           function (e1, e2) {
-              e1$query(deparse(substitute(e2)), substitute(e2), parent.frame())
+              e1$query(paste(deparse(substitute(e2)), collapse="\n"), substitute(e2), parent.frame())
           })
 
 connection <- function(ip, port) {
